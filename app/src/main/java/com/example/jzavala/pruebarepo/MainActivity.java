53aad7e5aba048ecaba8a09f74ea0d078521fdcc
AaronBL <abautista@ia.com.mx>
2018-03-02 12:35:42 -0600

    Agregar rama develop y nombre completo

--- app/src/main/java/com/example/jzavala/pruebarepo/MainActivity.java
+++ app/src/main/java/com/example/jzavala/pruebarepo/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        String modif= "Aaron";
+        String modif= "Aaron bautista";
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
